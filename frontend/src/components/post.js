5eba4b7ef65f2afb76edc94ccd73bf2446a57926
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-18 01:50:01 +0900

    판매하기(post) 구조

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -4,7 +4,72 @@ class Post extends Component {
     render() {
         return (
             <div className="post">
-                
+                <form>
+                    <table>
+                        <tr>
+                            <td>상품명</td>
+                            <td>
+                                <input type="text"></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>카테고리</td>
+                            <td>
+                                <div className="wrap-radio">
+                                    {
+                                        //input type="radio"
+                                    }
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>가격</td>
+                            <td>
+                                <input type="text"></input>
+                                <button>무료나눔</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>상품 정보</td>
+                            <td>
+                                <input type="text"></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>물건상태</td>
+                            <td>
+                                <div className="wrap-radio">
+                                    {
+                                        // input type="radio"
+                                    }
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>거래장소</td>
+                            <td>
+                                <input type="text"></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>교환 여부</td>
+                            <td>
+                                <div className="wrap-radio">
+                                    {
+                                        // input type="radio"
+                                    }
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>사진</td>
+                            <td>
+                                <input type="file"></input>
+                            </td>
+                        </tr>
+                    </table>
+                    <button className="post-button" type="submit">상품 등록하기</button>
+                </form>
             </div>
         );
     }
