db1a6b36577dcb5aadfa641db802af1f4839ce70
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-20 03:57:17 +0900

    상단 네비게이션, 판매하기 css 수정

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -25,11 +25,10 @@ class Nav extends Component {
                             <button type="submit">검색</button>
                         </form>
                     </div>
-                    <div className="nav-item">
+                    <div className="nav-item wrap-nav-item">
                         <NavLink to={"/post"} className="in-item">판매하기</NavLink>
                         <NavLink to={"/mypage"} className="in-item">마이페이지</NavLink>
                         <NavLink to={"/mirim-talk"} className="in-item">미림톡</NavLink>
-                        <NavLink to="/list" className="in-item">상품</NavLink>
                     </div>
                 </div>
             </div>
