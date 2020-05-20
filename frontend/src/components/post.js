e92bd597b6a349913c65d52c4390472319939192
Sookhee <s2018s38@e-mirim.hs.kr>
2020-05-21 03:04:22 +0900

    판매하기 css 수정 (선택지)

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -17,9 +17,20 @@ class Post extends Component {
                             <td>카테고리</td>
                             <td>
                                 <div className="wrap-radio">
-                                    {
-                                        //input type="radio"
-                                    }
+                                    <input type='radio' value='1-1' name='q1' id='radio1-1' onChange={this.handleOptionChangeQuiz1}/>
+                                    <label htmlFor='radio1-1'>의류잡화</label>
+                                    <input type='radio' value='1-2' name='q1' id='radio1-2' onChange={this.handleOptionChangeQuiz1}/>
+                                    <label htmlFor='radio1-2'>생활문구</label>
+                                    <input type='radio' value='1-3' name='q1' id='radio1-3' onChange={this.handleOptionChangeQuiz1}/>
+                                    <label htmlFor='radio1-3'>식료품</label>
+                                </div>
+                                <div className="wrap-radio">
+                                    <input type='radio' value='1-4' name='q1' id='radio1-4' onChange={this.handleOptionChangeQuiz1}/>
+                                    <label htmlFor='radio1-4'>전자제품</label>
+                                    <input type='radio' value='1-5' name='q1' id='radio1-5' onChange={this.handleOptionChangeQuiz1}/>
+                                    <label htmlFor='radio1-5'>문화예술</label>
+                                    <input type='radio' value='1-6' name='q1' id='radio1-6' onChange={this.handleOptionChangeQuiz1}/>
+                                    <label htmlFor='radio1-6'>스타굿즈</label>
                                 </div>
                             </td>
                         </tr>
@@ -40,9 +51,12 @@ class Post extends Component {
                             <td>물건상태</td>
                             <td>
                                 <div className="wrap-radio">
-                                    {
-                                        // input type="radio"
-                                    }
+                                    <input type='radio' value='2-1' name='q2' id='radio2-1' onChange={this.handleOptionChangeQuiz2}/>
+                                    <label htmlFor='radio2-1'>새 상품</label>
+                                    <input type='radio' value='2-2' name='q2' id='radio2-2' onChange={this.handleOptionChangeQuiz2}/>
+                                    <label htmlFor='radio2-2'>거의 새 상품</label>
+                                    <input type='radio' value='2-3' name='q2' id='radio2-3' onChange={this.handleOptionChangeQuiz2}/>
+                                    <label htmlFor='radio2-3'>중고</label>
                                 </div>
                             </td>
                         </tr>
@@ -56,9 +70,10 @@ class Post extends Component {
                             <td>교환 여부</td>
                             <td>
                                 <div className="wrap-radio">
-                                    {
-                                        // input type="radio"
-                                    }
+                                    <input type='radio' value='3-1' name='q3' id='radio3-1' onChange={this.handleOptionChangeQuiz3}/>
+                                    <label htmlFor='radio3-1'>가능</label>
+                                    <input type='radio' value='3-2' name='q3' id='radio3-2' onChange={this.handleOptionChangeQuiz3}/>
+                                    <label htmlFor='radio3-2'>불가능</label>
                                 </div>
                             </td>
                         </tr>
--- frontend/src/components/product-list.js
+++ frontend/src/components/product-list.js
@@ -3,14 +3,52 @@ import {NavLink} from "react-router-dom";
 import "../style/product.css";
 
 class ProductList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            productData: [
+                {
+                    id: 11,
+                },
+                {
+                    id: 22,
+                },
+                {
+                    id: 33,
+                },
+                {
+                    id: 44,
+                },
+                {
+                    id: 55,
+                },
+                {
+                    id: 66,
+                },
+                {
+                    id: 77,
+                },
+                {
+                    id: 88,
+                }
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="product-list">
                 <div className="wrap-product">
-                    <Item prod_id="11"/>
-                    <Item prod_id="22"/>
-                    <Item prod_id="33"/>
-                    <Item prod_id="44"/>
+                    {
+                    this.state.productData.map((product, i) => {
+                        return (
+                            <Item key={i}
+                                prod_id={product.id}
+                            />
+                            );
+                        })
+                    }
                 </div>
             </div>
         );
@@ -21,11 +59,13 @@ class Item extends Component {
     render() {
         return (
             <div className="item">
-                <NavLink to={"/product/" + this.props.prod_id} prod_id="11">
+                <NavLink to={"/product/" + this.props.prod_id} prod_id={this.props.prod_id}>
                     <img src="https://i.picsum.photos/id/20/300/200.jpg" className="img"></img>
-                    <div className="title">상품 명</div>
-                    <div className="seller">판매자</div>
-                    <div className="price">가격</div>
+                    <div className="wrap-content">
+                        <div className="title">상품 명</div>
+                        <div className="seller">판매자</div>
+                        <div className="price">가격</div>
+                    </div>
                 </NavLink>
             </div>
         );
