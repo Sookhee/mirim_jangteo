e0a93f28f80b41f42a9465d64fcfbd93c5b5d54d
Sagittta <js1verj@gmail.com>
2020-05-24 00:57:42 +0900

    add router, express-session module

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var storeRouter = require('./routes/store');
+const session = require('express-session');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const storeRouter = require('./routes/store');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,5 +41,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(session({
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24000 * 60 * 60
+  }
+}));
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,94 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+const models = require('../models');
+const crypto = require('crypto');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.redirect('/users/mypage');
+});
+
+router.get('/sign-up', function(req, res, next) {
+  res.render('users/signup');
+});
+
+router.post('/sign-up', async function(req, res, next) {
+  let body = req.body;
+
+  let inputPassword = body.password;
+  let result = models.user.create({
+    name: body.userName,
+    email: body.userEmail,
+    password: inputPassword
+  });
+
+  // 암호화
+  // let salt = Math.round(new Date().valueOf() * Math.random()) + "";
+  // let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest("hex");
+  // let result = models.user.create({
+  //   name: body.userName,
+  //   email: body.userEmail,
+  //   password: hashPassword,
+  //   salt: salt
+  // });
+
+  res.redirect('/users/sign-up')
+
+});
+
+router.get('/sign-in', function(req, res, next) {
+  let session = req.session;
+  
+  res.render('users/login', {
+    session: session
+  });
+});
+
+router.post('/sign-in', async function(req, res, next) {
+  let body = req.body;
+
+  let result = await models.user.findOne({
+    where: {
+      email: body.email
+    }
+  });
+
+  let dbPassword = result.dataValues.password;
+  let inputPassword = body.password;
+
+  if (dbPassword = inputPassword) {
+    console.log('비밀번호 일치');
+
+    req.session.email = body.userEmail;
+    
+    res.redirect('/users');
+    
+  //  view 에서 if (session.email) { } else { } 사용
+
+  } else {
+    console.log('비밀번호 불일치');
+    res.redirect('/users/sign-in');
+  }
+
+});
+
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+  res.clearCookie('sid');
+
+  res.redirect('/users/sign-in');
+});
+
+router.get('/mypage', function(req, res, next) {
+  if (req.cookies) {
+    console.log(req.cookies);
+    res.render('users/mypage');
+  } else {
+    console.log('로그인하세요');
+    res.redirect('/users/sign-in');
+  }
 });
 
 module.exports = router;
