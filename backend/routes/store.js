3ef43df2cfb2e6df3e1f5b130e4ce67cbcfdd1df
Sagittta <js1verj@gmail.com>
2020-05-27 02:59:21 +0900

    edit sequelizing main, banner, search list, post, product detail

--- backend/routes/store.js
+++ backend/routes/store.js
@@ -10,113 +10,106 @@ var {Product} = require('../models');
 var {Like} = require('../models');
 var {Banner} = require('../models');
 
-/* GET users listing. */
-router.get('/list', function(req, res, next) {
-    res.redirect('/store/list/1');
-});
+// 카테고리별 인기있는 상품 8개
+router.get('/popular/:category', function(req, res, next) {
+    // 카테고리 불러오기
+    var category = req.params.category;
 
-// 고민 중: /list/:category or ajax 사용
-router.get('/list', function(req, res, next) {
-    // 현재 페이지 읽어옴.
-    // var page = req.params.page;
-    // TODO: category 읽어오기
-    var category = 1;
-
-    var banners, products;
-    Banner.find({order: 'createdAt DESC'})
-        .then((banner) => {
-            banners = {
-                id: banner.banner_id,
-                image: banner.banner_img
-            }
-        });
-
-    Product.find({
+    Product.findAll({
         where: {
             product_status: 0,
             category: category
         },
         order: 'product_count DESC',
         limit: 8
-    }).then((product) => {
-        products = {
-            id: product.product_id,
-            title: product.product_title,
-            seller: product.name,
-            price: product.product_price,
-            image: product.product_img
-        }
-    });
-
-    res.json({
-        banner: banners,
-        product: products
-    });
-});
-
-router.get('/list/:search', function(req, res, next) {
-    var search = req.body.search;
-    res.redirect('/list/' + search +'/1');
-});
-
-router.get('/list/:search/:page', function(req, res, next) {
-    var page = req.params.page;
-    var search = req.params.search;
-    // order = 0 이면 인기순, order = 1 이면 최신순
-    var order = 0;
-
-    var orders, products;
-
-    if (order === 0)
-        orders = 'product_count';
-    else
-        orders = 'createdAt';
-
-    Product.findAll({
-        where: {
-            product_title: {
-                [Op.like]: '%' + search + '%'
-            }
-        },
-        order: orders + 'DESC'
-    }).then((product) => {
-        products = {
-            id: product.product_id,
-            title: product.product_title,
-            seller: product.name,
-            price: product.product_price,
-            image: product.product_img
-        };
-
+    }).then((products) => {
         res.json({
-            products: products,
-            page: page,
-            length: products.length - 1,
-            page_num: 20,
-            pass: true
+            id: products.product_id,
+            title: products.product_title,
+            seller: products.name,
+            price: products.product_price,
+            image: products.product_img
         });
-    }).catch( err => {
-        console.error('err: ' + err);
-    });
-});
-
-router.get('/post', function(req, res, next) {
-    res.render('write');
-});
-
-router.post('/post', function(req, res, next) {
-    // TODO: user 세션에서 가져오기
-    const user = 1;
-    let user_name;
-
-    User.find({
-        where: {member_id: user}
-    }).then((users) => {
-        user_name = users.name;
-    }).catch(err => {
+    }).catch((err) => {
         console.error('err: ' + err);
     });
     
+});
+
+// 배너
+router.get('/banner/:id', function(req, res, next) {
+    // 배너 아이디 불러와서 하나하나 가져오기
+    var id = req.params.banner_id;
+
+    Banner.findAll({
+        where: {
+            banner_id: id
+        },
+        order: 'createdAt DESC'
+    }).then((banner) => {
+        res.json({
+            id: banner.banner_id,
+            image: banner.banner_img
+        });
+    }).catch(err => {
+        console.error('err: ' + err);
+    })
+});
+
+// 검색
+router.get('/search/:keyword/:page', function(req, res, next) {
+    // 키워드랑 현재 무슨 페이지인지 가져오기
+    var keyword = req.params.keyword;
+    var page = req.params.page;
+    // 인기순인지 최신순인지 불러오기
+    // TODO: order = 0: 인기순, order = 1: 최신순
+    // 소녀나라 예시: <a href="/shop/list.php?page=1&cate=0104&orby=1">신상품</a>
+    var order = 0;
+
+    Product.findAll({
+        where: {
+            [Op.or]: [
+                {product_title: {
+                    [Op.like]: '%' + keyword + '%'
+                    }},
+                {product_content: {
+                    [Op.like]: '%' + keyword + '%'
+                    }}
+            ]
+        },
+        order: 'product_count DESC'
+    }).then((products) => {
+        res.json({
+            id: products.product_id,
+            title: products.product_title,
+            seller_id: products.member_id,
+            seller: products.name,
+            content: products.product_content,
+            image: products.product_img,
+            page: page,
+            length: products.length - 1,
+            limit: 20,
+            pass: true
+        });
+    }).catch(err => {
+        console.error('err: ' + err);
+    });
+});
+
+// 상품 게시
+router.post('/post', function(req, res, next) {
+    // TODO: member_id 는 세션에서 가져오기
+    const member_id = 'jyomi';
+    let member_name = '';
+
+    User.find({
+        where: {member_id: member_id}
+    }).then((user) => {
+        member_name = user.name
+    }).catch(err => {
+        console.error('err: ' + err);
+    });
+
     const title = req.body.title;
     const category = req.body.category;
     const price = req.body.price;
@@ -126,11 +119,9 @@ router.post('/post', function(req, res, next) {
     const swap = req.body.swap;
     const image = req.body.image;
 
-    let product_id;
-
     Product.create({
-        member_id: user,
-        name: user_name,
+        member_id: member_id,
+        name: member_name,
         product_title: title,
         product_content: content,
         category: category,
@@ -139,26 +130,25 @@ router.post('/post', function(req, res, next) {
         product_img: image,
         product_place: place,
         product_swap: swap
-    }).then(result => {
-        product_id = result.product_id;
-        // res.json(result);
-        res.redirect('/detail/' + product_id);
+    }).then((result) => {
+        res.json({
+            product_id: result.product_id
+        });
     }).catch(err => {
-        console.error('err: ' + err);
+        console.error('err: ' + error);
     });
 
 });
 
-router.get('/detail/:index', function(req, res, next) {
-    var index = req.params.product_id;
+// 상품 자세히 보기
+router.get('/detail/:id', function (req, res, next) {
+    var id = req.params.product_id;
 
-    var products;
-
-    Product.find({
-        where: {product_id: index}
+    Product.findOne({
+        where: {product_id: id}
     }).then((product) => {
-        products = {
-            id: index,
+        res.json({
+            id: id,
             seller: product.name,
             title: product.product_title,
             content: product.product_content,
@@ -170,13 +160,10 @@ router.get('/detail/:index', function(req, res, next) {
             count: product.product_count,
             place: product.product_place,
             swap: product.product_swap
-        };
-        res.json(products);
+        });
     }).catch(err => {
         console.error('err: ' + err);
     });
-
-    // res.render('detail');
 });
 
 module.exports = router;
