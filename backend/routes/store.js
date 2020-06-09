2829c9a69145b325a633e2c1c12613f31cfa2844
Sagittta <js1verj@gmail.com>
2020-06-09 22:48:39 +0900

    디테일 화면에 찜 여부 보내기 (like:0 -> 찜 X, like:1 -> 찜 O)

--- backend/routes/store.js
+++ backend/routes/store.js
@@ -78,7 +78,6 @@ router.get('/search/:keyword/:order/:page', function(req, res, next) {
     });
 });
 
-
 // 상품 게시 (get 으로 테스트했을 때 데이터 들어감)
 router.get('/post', function(req, res, next) {
     // TODO: member_id 는 세션에서 가져오기
@@ -120,13 +119,24 @@ router.get('/detail/:id', function (req, res, next) {
     let id = req.params.id;
     id *= 1;
 
-    const query = 'SELECT * FROM products WHERE id = ?';
+    let query = 'SELECT * FROM products WHERE id = ?';
 
     const productList = [];
     connection.query(query, [id], (err, result) => {
         if (err) {
             return res.send(err);
         } else {
+            let like = 0;
+            query = "SELECT id FROM like_lists WHERE member_id = ? AND product_id = ?";
+            connection.query(query, [result[0].member_id, result[0].id], (err, result2) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    if (result2) {
+                        like = 1;
+                    }
+                }
+            });
             const strJSON = JSON.stringify({
                 id: id,
                 member_id: result[0].member_id,
@@ -140,13 +150,12 @@ router.get('/detail/:id', function (req, res, next) {
                 product_img: result[0].product_img,
                 product_swap: result[0].product_swap,
                 createdAt: result[0].createdAt,
-                updatedAt: result[0].updatedAt
-            })
-            res.send(strJSON)
-            // for (let i = 0; i < result.length; i++) {
-            //     productList[i] = result[i];
-            // }
-            // res.send(productList);
+                updatedAt: result[0].updatedAt,
+                like: like
+            });
+            console.log(strJSON);
+            // productList.push(strJSON);
+            res.send(strJSON);
         }
     });
 });
